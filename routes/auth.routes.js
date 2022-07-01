0fb6032f56b3779a52a69f3c58d4e7b074d92ad9
matrizaev22 <matrizaevsarvar@gmail.com>
2022-07-01 18:45:53 +0500

    auth, database

---
+++ routes/auth.routes.js
@@ -0,0 +1,17 @@
+import { Router} from 'express'
+
+const router = Router()
+
+router.post('/login', (req, res) =>{
+    
+    
+    res.send('Login')
+})
+
+router.post('/register', (req, res) =>{
+    
+    
+    res.send('Register')
+})
+
+export default router
