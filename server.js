08fa15f234cdb02e78001667810e3c40a4c84099
matrizaev22 <matrizaevsarvar@gmail.com>
2022-07-01 17:39:54 +0500

    git added

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log('Server is running on http://localhost:8080');
+})
