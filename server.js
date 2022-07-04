2ef41e92d67dbb0b8f25e89cc8c39d60665ba363
matrizaev22 <matrizaevsarvar@gmail.com>
2022-07-04 17:04:42 +0500

    up

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import authRoutes from './routes/auth.routes.js'
 
 dotenv.config()
 
@@ -9,6 +10,8 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+app.use(authRoutes)
 
 
 
